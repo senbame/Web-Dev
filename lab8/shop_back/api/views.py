d3440e3991262127a45eccfd07b95e210bcb8467
senbame <dastannursultan2006@gmail.com>
2026-03-30 00:02:24 +0500

    lab8

---
+++ lab8/shop_back/api/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Product, Category
+# Create your views here.
+
+# All List of Products
+def product_list(request):
+    products = Product.objects.all()
+    data = []
+
+    for p in products:
+        data.append({
+            "id": p.id,
+            "name": p.name,
+            "price": p.price,
+            "description": p.description,
+            "count": p.count,
+            "is_active": p.is_active,
+            "category": p.category.id
+        })
+
+    return JsonResponse(data, safe=False)
+
+# One Product
+
+def product_detail(request, id):
+    p = Product.objects.get(id=id)
+
+    data = {
+        "id": p.id,
+        "name": p.name,
+        "price": p.price,
+        "description": p.description,
+        "count": p.count,
+        "is_active": p.is_active,
+        "category": p.category.id
+    }
+
+    return JsonResponse(data)
+
+# All Categories
+
+def category_list(request):
+    categories = Category.objects.all()
+    data = []
+
+    for c in categories:
+        data.append({
+            "id": c.id,
+            "name": c.name
+        })
+
+    return JsonResponse(data, safe=False)
+
+# One Category
+
+def category_detail(request, id):
+    c = Category.objects.get(id=id)
+
+    data = {
+        "id": c.id,
+        "name": c.name
+    }
+
+    return JsonResponse(data)
+
+# Products by Category
+
+def products_by_category(request, id):
+    products = Product.objects.filter(category_id=id)
+    data = []
+
+    for p in products:
+        data.append({
+            "id": p.id,
+            "name": p.name,
+            "price": p.price
+        })
+
+    return JsonResponse(data, safe=False)
